6df112b5a4643edceb3d394f4ac2f19eaf970451
hiroaki-com <comukichi@gmail.com>
2022-12-16 23:57:03 +0900

    [add]refs#30 settings-media-staticfiles-configuration

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static_local" ] # Todo:static_local を暫定的に指定
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media_local" # Todo:media_local を暫定的に指定
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
