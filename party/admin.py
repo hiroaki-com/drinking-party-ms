e842c16a223c8729fd045bd732feaa82a7fc4803
hiroaki-com <comukichi@gmail.com>
2022-12-27 01:09:26 +0900

    add: 管理画面の利便性向上

--- party/admin.py
+++ party/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
 from .models import Party
- 
+
+
+class PartyAdmin(admin.ModelAdmin):
+    list_display = ('id','title')
+    list_display_links = ('id','title')
+
 admin.site.register(Party)
