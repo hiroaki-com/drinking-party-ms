03e2ffdcc21e0347eaa84d47e9e1bdf15a8a9a5b
hiroaki-com <comukichi@gmail.com>
2022-12-27 14:04:05 +0900

    fix: フォームからのPOSTがデータベースへ反映されない件の対応

--- party/forms.py
+++ party/forms.py
@@ -8,6 +8,8 @@ class PartyForm(ModelForm):
     class Meta:
         model = Party
         fields = [
-        'title', 'date', 'time', 'restaurant', 'address', 'url',
+        'user', 'title', 'date', 'time', 'restaurant', 'address', 'url',
         'subscriber', 'fee','comment','create_dt','mod_dt',
         ]
+
+# TODO: NULL制約により、user,create_dt,mod_dt の登録が必須となっているため、対処する
