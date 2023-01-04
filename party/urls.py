a2260be3318eb9e031bd5e8f3e22efd31380d899
hiroaki-com <comukichi@gmail.com>
2023-01-04 14:06:28 +0900

    add:#110-- implement party delete feature

--- party/urls.py
+++ party/urls.py
@@ -8,6 +8,7 @@ app_name = 'party'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('create_party/', views.PartyCreateView.as_view(), name='create_party'),
+    path('delete_party/<int:pk>', views.PartyDeleteView.as_view(), name='delete_party'),
     path('party_detail/<int:pk>', views.PartyDetailView.as_view(), name='party_detail'),
     path('join_for_party/', views.join_for_party, name='join_for_party'),
     path('not_join_for_party/', views.not_join_for_party, name='not_join_for_party'),
